752e39227c667eed07244ad9e887a3daa41e01e8
Robert Adamyan <robert.adamyan1@gmail.com>
2018-02-07 04:29:29 +0400

    add: setup and primitives

---
+++ src/primitives.js
@@ -0,0 +1,26 @@
+typeof 'a' // string
+typeof 5 // number
+typeof undefined // undefined
+typeof boolean // boolean
+typeof null // object, inconsistency
+
+const f = x => x
+typeof f // function
+
+const sym = Symbol(35)
+typeof sym // symbol
+
+'1' == 1 // true
+'1' === 1 // false
+
+0 == false // true
+1 == true // true
+
+0 === false // false
+1 === true // false
+
+'1' ==  true // true
+'0' ==  false // true
+
+'1' === true // false
+'0' === false // false
