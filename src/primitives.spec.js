a5fb84d02f5409df5b67e706a3bd2ca66a2f90a2
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2018-02-08 20:14:41 +0400

    again fixing your life Rob jan

--- src/primitives.spec.js
+++ src/primitives.spec.js
@@ -1,44 +1,44 @@
-const mocha = require('mocha')
-const assert = require('chai').assert
+const mocha = require('mocha');
+const assert = require('chai').assert;
 
 describe('null', function() {
   it('typeof null should return [something], not null, for historical reasons', function() {
-    assert.strictEqual(typeof null, 'null')
-  })
-})
+    assert.strictEqual(typeof null, 'null');
+  });
+});
 
 describe('1', function() {
   it(`'1' as a string is strictEqual to 1 as a number due to type coercion`, function() {
-    assert.strictEqual('1', 1)
-  })
-})
+    assert.strictEqual('1', 1);
+  });
+});
 
 describe('1', function() {
   it(`'1' as a string is not equal to the boolean true`, function() {
-    assert.notEqual('1', true)
-  })
-})
+    assert.notEqual('1', true);
+  });
+});
 
 describe('1', function() {
   it(`1 as a number is not equal to the boolean true`, function() {
-    assert.notEqual(1, true)
-  })
-})
+    assert.notEqual(1, true);
+  });
+});
 
 describe('0', function() {
   it(`'0' as a string is strict equal to the boolean false due to type coercion`, function() {
-    assert.strictEqual('0', false)
-  })
-})
+    assert.strictEqual('0', false);
+  });
+});
 
 describe('0', function() {
   it(`0 as a number is equal to the boolean true due to type coercion`, function() {
-    assert.equal(0, true)
-  })
-})
+    assert.equal(0, true);
+  });
+});
 
 describe('0', function() {
   it(`0 as a number is strict equal to the boolean false because strict equal doesn't cast types`, function() {
-    assert.strictEqual(0, false)
-  })
-})
+    assert.strictEqual(0, false);
+  });
+});
