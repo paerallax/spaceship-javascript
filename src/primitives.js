187dc471f4013fa0734cd55b221e6e10c391e70e
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2018-02-08 20:13:34 +0400

    fixing your life

--- src/primitives.js
+++ src/primitives.js
@@ -1,29 +1,29 @@
-typeof 'a' // string
-typeof 5 // number
-typeof undefined // undefined
-typeof false // boolean
-typeof null // object, inconsistency
+typeof 'a'; // string
+typeof 5; // number
+typeof undefined; // undefined
+typeof false; // boolean
+typeof null; // object, inconsistency
 
-const f = x => x
-typeof f // function
+const f = x => x;
+typeof f; // function
 
-const sym = Symbol(35)
-typeof sym // symbol
+const sym = Symbol(35);
+typeof sym; // symbol
 
-'1' == 1 // true
-'1' === 1 // false
+'1' == 1; // true
+'1' === 1; // false
 
-0 == false // true
-1 == true // true
+0 == false; // true
+1 == true; // true
 
-0 === false // false
-1 === true // false
+0 === false; // false
+1 === true; // false
 
-'1' ==  true // true
-'0' ==  false // true
+'1' ==  true; // true
+'0' ==  false; // true
 
-'1' === true // false
-'0' === false // false
+'1' === true; // false
+'0' === false; // false
 
-null == undefined // true
-null === undefined // false
+null == undefined; // true
+null === undefined; // false
