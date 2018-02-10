80abe345de2c83a69e44753f87f330f717f8f081
MahtaMR <zohreh.torabian7366@gmail.com>
2018-02-10 15:11:01 +0400

    fixed

--- src/primitives.spec.js
+++ src/primitives.spec.js
@@ -2,43 +2,43 @@ const mocha = require('mocha')
 const assert = require('chai').assert
 
 describe('null', function() {
-  it('typeof null should return [something], not null, for historical reasons', function() {
-    assert.strictEqual(typeof null, 'null')
+  it('typeof null should return [object], not null, for historical reasons', function() {
+    assert.strictEqual(typeof null, 'object')
   })
 })
 
 describe('1', function() {
-  it(`'1' as a string is strictEqual to 1 as a number due to type coercion`, function() {
-    assert.strictEqual('1', 1)
+  it(`'1' as a string is equal to 1 as a number due to type coercion`, function() {
+    assert.equal('1', 1)
   })
 })
 
 describe('1', function() {
-  it(`'1' as a string is not equal to the boolean true`, function() {
-    assert.notEqual('1', true)
+  it(`'1' as a string is equal to the boolean true`, function() {
+    assert.equal('1', true)
   })
 })
 
 describe('1', function() {
-  it(`1 as a number is not equal to the boolean true`, function() {
-    assert.notEqual(1, true)
+  it(`1 as a number is equal to the boolean true`, function() {
+    assert.equal(1, true)
   })
 })
 
 describe('0', function() {
-  it(`'0' as a string is strict equal to the boolean false due to type coercion`, function() {
-    assert.strictEqual('0', false)
+  it(`'0' as a string is equal to the boolean false due to type coercion`, function() {
+    assert.equal('0', false)
   })
 })
 
 describe('0', function() {
-  it(`0 as a number is equal to the boolean true due to type coercion`, function() {
-    assert.equal(0, true)
+  it(`0 as a number is equal to the boolean false due to type coercion`, function() {
+    assert.equal(0, false)
   })
 })
 
 describe('0', function() {
-  it(`0 as a number is strict equal to the boolean false because strict equal doesn't cast types`, function() {
-    assert.strictEqual(0, false)
+  it(`0 as a number is equal to the boolean false because strict equal doesn't cast types`, function() {
+    assert.equal(0, false)
   })
 })
